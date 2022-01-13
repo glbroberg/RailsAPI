4514bfde61c46b670f8ca3fb2c3c278c7dac6ed7
Garrett Broberg <glbroberg@gmail.com>
2022-01-13 14:28:25 -0500

    added article rspec tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#validations' do
+    let (:article) {build(:article)}  # ':article' => factory name - from factorybot
+    it "Test that factory is valid" do
+      # article = create(:article) -> actually creates and writes article to the DB, which is not nessicary to test the factory
+      
+      expect(article).to be_valid # atricle.valid? = true
+    end
+
+    it "has an invalid title" do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+  end
 end
